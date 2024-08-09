8c160143837215f03014cd38732b1a9fe07a071e
Muhamad Adib Minanurohman <inanadib084@gmail.com>
2024-08-09 14:43:03 +0700

    playlis 6 view data

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -12,8 +12,8 @@
                         <a href="/"
                             class="{{ request()->is('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium "
                             aria-current="page">Home</a>
-                        <a href="/blog"
-                            class="{{ request()->is('blog') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium">Blog</a>
+                        <a href="/posts"
+                            class="{{ request()->is('posts') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium">Blog</a>
                         <a href="/about"
                             class="{{ request()->is('about') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}}  rounded-md px-3 py-2 text-sm font-medium">About</a>
                         <a href="/contact"
@@ -85,14 +85,14 @@
     <div x-show="isOpen" class="md:hidden" id="mobile-menu">
         <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-            <a href="/" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
+            <a href="/" class="{{ request()->is('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} block rounded-md px-3 py-2 text-base font-medium"
                 aria-current="page">Home</a>
-            <a href="/blog"
-                class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Blog</a>
+            <a href="/posts"
+                class="{{ request()->is('posts') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} block rounded-md px-3 py-2 text-base font-medium">Blog</a>
             <a href="/about"
-                class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
+                class="{{ request()->is('about') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} block rounded-md px-3 py-2 text-base font-medium">About</a>
             <a href="/contact"
-                class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                class="{{ request()->is('contact') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} block rounded-md px-3 py-2 text-base font-medium">Contact</a>
         </div>
         <div class="border-t border-gray-700 pb-3 pt-4">
             <div class="flex items-center px-5">
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,83 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'id' => '1',
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Adib Minanurohman',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis dignissimos dolores cumque est fuga
+            voluptatum exercitationem minima illum itaque? Sit, aliquid. Non assumenda cum dicta atque maxime. Sunt,
+            repellendus laudantium.'
+        ],
+        [
+            'id' => '2',
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Adib Minanurohman',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis dignissimos dolores cumque est fuga
+            voluptatum exercitationem minima illum itaque? Sit, aliquid. Non assumenda cum dicta atque maxime. Sunt,
+            repellendus laudantium.'
+        ],
+        [
+            'id' => '3',
+            'slug' => 'judul-artikel-3',
+            'title' => 'Judul Artikel 3',
+            'author' => 'Adib Minanurohman',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis dignissimos dolores cumque est fuga
+            voluptatum exercitationem minima illum itaque? Sit, aliquid. Non assumenda cum dicta atque maxime. Sunt,
+            repellendus laudantium.'
+        ]
+    ]],);
 });
 Route::get('/about', function () {
     return view('about', ['title' => 'About']);
 });
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
+});
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => '1',
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Adib Minanurohman',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis dignissimos dolores cumque est fuga
+            voluptatum exercitationem minima illum itaque? Sit, aliquid. Non assumenda cum dicta atque maxime. Sunt,
+            repellendus laudantium.'
+        ],
+        [
+            'id' => '2',
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Adib Minanurohman',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis dignissimos dolores cumque est fuga
+            voluptatum exercitationem minima illum itaque? Sit, aliquid. Non assumenda cum dicta atque maxime. Sunt,
+            repellendus laudantium.'
+        ],
+        [
+            'id' => '3',
+            'slug' => 'judul-artikel-3',
+            'title' => 'Judul Artikel 3',
+            'author' => 'Adib Minanurohman',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis dignissimos dolores cumque est fuga
+            voluptatum exercitationem minima illum itaque? Sit, aliquid. Non assumenda cum dicta atque maxime. Sunt,
+            repellendus laudantium.'
+        ]
+    ];
+
+    $post = Arr::first($posts, function($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+    
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
