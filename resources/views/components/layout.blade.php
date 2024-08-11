4459868a2781373612ff4b28b5f478ff21c28056
Muhamad Adib Minanurohman <inanadib084@gmail.com>
2024-08-11 21:48:04 +0700

    playlist13

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -18,7 +18,7 @@
         <x-header>{{ $title }}</x-header>
         <main>
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                {{ $slot }}
+                {{ $slot }} 
             </div>
         </main>
     </div>
