16082b298abc0d950cca9e86912511ab8ccb39a2
Muhamad Adib Minanurohman <inanadib084@gmail.com>
2024-08-13 16:23:27 +0700

    playlist14

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,8 @@ Route::get('/', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
+    $posts = Post::all();
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 Route::get('/about', function () {
     return view('about', ['title' => 'About']);
@@ -25,10 +26,8 @@ Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 Route::get('/authors/{user:username}', function (User $user) {
-
     return view('posts', ['title' => count($user->posts) . ' Article by '. $user->name, 'posts'=> $user->posts]);
 });
 Route::get('/categories/{category:slug}', function (Category $category) {
-
     return view('posts', ['title' => 'Article in: '.$category->name, 'posts'=> $category->posts]);
 });
