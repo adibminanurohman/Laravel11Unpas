2fe38d0ee050fc2a428259d9c3f3e39e942e4d73
Muhamad Adib Minanurohman <inanadib084@gmail.com>
2024-08-14 15:56:51 +0700

    playlist16

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@ Route::get('/', function () {
 });
 
 Route::get('/posts', function () {
-    $posts = Post::all();
-    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
+    return view('posts', ['title' => 'Blog', 'posts' => Post::filter(request(['search', 'category', 'author']))->latest()->get()]);
 });
+
 Route::get('/about', function () {
     return view('about', ['title' => 'About']);
 });
@@ -26,8 +26,8 @@ Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 Route::get('/authors/{user:username}', function (User $user) {
-    return view('posts', ['title' => count($user->posts) . ' Article by '. $user->name, 'posts'=> $user->posts]);
+    return view('posts', ['title' => count($user->posts) . ' Article by ' . $user->name, 'posts' => $user->posts]);
 });
 Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('posts', ['title' => 'Article in: '.$category->name, 'posts'=> $category->posts]);
+    return view('posts', ['title' => 'Article in: ' . $category->name, 'posts' => $category->posts]);
 });
